5e0d98597d7ffac27f03532fe902f16301a5ba9e
entymon <pawel.olejniczak.i@gmail.com>
2021-12-05 13:07:24 +0000

    add new endpoint

--- src/components/harvard-art-museum/hardvard-art-museum.controller.ts
+++ src/components/harvard-art-museum/hardvard-art-museum.controller.ts
@@ -16,7 +16,7 @@ export default class HarvardArtMuseum extends BaseApi {
     public register(express: Application): void {
         express.use('/api/harvard-art-museum', this.router)
         this.router.get('/prints/:page?', this.getPrintsByPage)
-        // this.router.get('/prints/:objectId', this.getPrintById)
+        this.router.get('/prints/:objectId', this.getPrintById)
     }
 
     /**
@@ -58,18 +58,19 @@ export default class HarvardArtMuseum extends BaseApi {
      * @param res 
      * @param next 
      */
-    // public getPrintById(req: Request, res: Response, next: NextFunction): void {
-    //     try {
-    //         axios.get(`https://api.harvardartmuseums.org/object/${req.params.objectId}`, {
-    //             query: {
-    //                 apikey: "c28e4be0-4c0e-11ea-90d6-25d9a9fe80fc"
-    //             }
-    //         }).on("complete", function(data, response) {
-    //             res.locals.data = data
-    //             responseHandler.json(res)
-    //         });
-    //     } catch (error) {
-    //         next(error)
-    //     }
-    // } 
+    public getPrintById(req: Request, res: Response, next: NextFunction): void {
+        axios.get(`https://api.harvardartmuseums.org/object/${req.params.objectId}`, {
+            params: {
+                apikey: "c28e4be0-4c0e-11ea-90d6-25d9a9fe80fc"
+            }
+        })
+        .then(function (response) {
+            console.log(response.data);
+            res.locals.data = response.data
+            responseHandler.json(res)
+        })
+        .catch(function (error) {
+            next(error);
+        })
+    } 
 }
